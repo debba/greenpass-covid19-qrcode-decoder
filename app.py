36583a3ef5e945320eb497ecace6ec4d083ec599
debba <andrea@emailchef.com>
2021-06-25 16:02:21 +0200

    small changes

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from PIL import Image
 from base45 import b45decode
 from zlib import decompress
 from flynn import decoder as flynn_decoder
-from urllib.request import urlopen
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template
 from os.path import splitext
 from flask_sslify import SSLify
 from flask_babel import Babel, gettext
 import os
-import json
+from lib.datamapper import DataMapper as data_mapper
 
 is_prod = os.environ.get('PRODUCTION', None)
 ga_id = os.environ.get('GA_ID', None)
@@ -30,9 +29,11 @@ app.config['LANGUAGES'] = {
 }
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
+
 
 if is_prod:
     sslify = SSLify(app)
@@ -40,25 +41,8 @@ if is_prod:
 
 @app.context_processor
 def inject_user():
-    return dict(github_project=app.config['GITHUB_PROJECT'], is_prod=is_prod, ga_id=ga_id, app_name=gettext('Green Pass COVID-19 QRCode Decoder'))
-
-
-def recursive_save_data(data, schema, level=0):
-    for key, value in data.items():
-        description = schema[key].get('title') or schema[key].get('description') or key
-        description, _, _ = description.partition(' - ')
-        if type(value) is dict:
-            app.converted_schema += '<p>' + ('&nbsp;' * level) + '<strong>' + description + '</strong>' + '</p>'
-            _, _, sch_ref = schema[key]['$ref'].rpartition('/')
-            recursive_save_data(value, app.glb_schema['$defs'][sch_ref]['properties'], level + 1)
-        elif type(value) is list:
-            app.converted_schema += '<p>' + ('&nbsp;' * level) + '<strong>' + description + '</strong>' + '</p>'
-            _, _, sch_ref = schema[key]['items']['$ref'].rpartition('/')
-            for v in value:
-                recursive_save_data(v, app.glb_schema['$defs'][sch_ref]['properties'], level + 1)
-        else:  # value is scalar
-            app.converted_schema += '<p>' + ('&nbsp;' * level) + '<strong>' + description + '</strong>' + ':' + str(
-                value) + '</p>'
+    return dict(github_project=app.config['GITHUB_PROJECT'], is_prod=is_prod, ga_id=ga_id,
+                app_name=gettext('Green Pass COVID-19 QRCode Decoder'))
 
 
 @app.route('/', methods=['GET'])
@@ -81,13 +65,8 @@ def qrdata():
         qrcode_data = decompress(b45decode(qr_decoded))
         (_, (header_1, header_2, cbor_payload, sign)) = flynn_decoder.loads(qrcode_data)
         data = flynn_decoder.loads(cbor_payload)
-
-        sch = urlopen(app.config['DCC_SCHEMA'])
-        app.glb_schema = json.load(sch)
-
-        recursive_save_data(data[-260][1], app.glb_schema['properties'])
-
-        return render_template('data.html', data=app.converted_schema)
+        dm = data_mapper(data, app.config['DCC_SCHEMA'])
+        return render_template('data.html', data=dm.convert_json())
     return render_template('error.html', error='UPLOAD_IMAGE_WITH_NO_NAME')
 
 
---
+++ lib/datamapper.py
@@ -0,0 +1,61 @@
+import json
+from urllib.request import urlopen
+
+class DataMapperError(Exception):
+    pass
+
+
+class DataMapper:
+    qr_data = None
+    schema = None
+
+    json = ''
+    new_json = {}
+
+    def _save_json(self, data, schema, level=0, json = {}):
+        for key, value in data.items():
+            description = schema[key].get('title') or schema[key].get('description') or key
+            description, _, _ = description.partition(' - ')
+            if type(value) is dict:
+                print('DICT')
+
+                self.json += '<p>' + ('&nbsp;' * level) + '<strong>' + description + '</strong>' + '</p>'
+                _, _, sch_ref = schema[key]['$ref'].rpartition('/')
+                self._save_json(value, self.schema['$defs'][sch_ref]['properties'], level + 1)
+            elif type(value) is list:
+                print('LL')
+
+                self.json += '<p>' + ('&nbsp;' * level) + '<strong>' + description + '</strong>' + '</p>'
+                _, _, sch_ref = schema[key]['items']['$ref'].rpartition('/')
+                for v in value:
+                    self._save_json(v, self.schema['$defs'][sch_ref]['properties'], level + 1)
+            else:  # value is scalar
+                print('SC')
+                self.json += '<p>' + ('&nbsp;' * level) + '<strong>' + description + '</strong>' + ':' + str(
+                    value) + '</p>'
+        return json
+
+    def __set_schema(self, schema_url):
+        sch = urlopen(schema_url)
+        self.schema = json.load(sch)
+
+    def __init__(self, qr_data, schema_url, params_string=False):
+
+        i = -260
+        j = 1
+
+        if params_string:
+            i = str(i)
+            j = str(j)
+
+        self.json = ''
+        self.qr_data = qr_data[i][j]
+        self.__set_schema(schema_url)
+
+    def convert_json(self):
+        if self.qr_data is None:
+            raise DataMapperError("QR_DATA_IS_WRONG")
+        if self.schema is None:
+            raise DataMapperError("SCHEMA_IS_WRONG")
+        self._save_json(self.qr_data, self.schema['properties'])
+        return self.json
